9ff180d0f98ecdd1ab448faa8345f076f6f4283f
gnidinger <gcnb87@gmail.com>
2023-02-28 10:54:30 +0900

    first commit

---
+++ src/main/java/backend/GetabeerRebuildApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetabeerRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetabeerRebuildApplication.class, args);
+	}
+
+}
